6942ca5cb11b953080394fc28fdf4d430b534b74
Xavier Davis <xdavis1@icloud.com>
2019-10-24 21:40:59 -0700

    bulding on the html pages

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// Potential Friends || Dates (DATA)
+// =============================================================
+
+let friends = [
+    {
+    "name": "George clooney",
+    "photo":"https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_634x1024-190514063109-634-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=inside|900:auto&output-quality=90",
+    "score":[
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5"
+    ]
+    },
+    
+]
--- app/data/public/routing/server.js
+++ app/data/public/routing/server.js
@@ -12,28 +12,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Pretend Friends (DATA)
-// =============================================================
-
-let friends = [
-    {
-    "name": "George clooney",
-    "photo":"https://akns-images.eonline.com/eol_images/Entire_Site/2019414/rs_634x1024-190514063109-634-George-Clooney-Batman-LT-051419-GettyImages-607408286.jpg?fit=inside|900:auto&output-quality=90",
-    "score":[
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5"
-    ]
-    },
-    
-]
 
 // Routes
 // =============================================================
